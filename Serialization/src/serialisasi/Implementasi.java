786b038269fa2dd18ab45ce94b4d2fa472234061
ilhamakhsan <50404822+ilhamakhsan@users.noreply.github.com>
2020-03-19 12:23:58 +0000

    Add files via upload

---
+++ Serialization/src/serialisasi/Implementasi.java
@@ -0,0 +1,14 @@
+
+package serialisasi;
+
+/**
+ *
+ * @author Ilham 
+ */
+public class Implementasi {
+    public static void main(String[] args) {
+        MainForm mfrm = new MainForm();
+        mfrm.setVisible(true);
+    }
+    
+}
